bdc308eafb2786396a93fcc48d53f4b1e41b9d33
Brent Labasan <BrentLabasan@gmail.com>
2020-04-18 14:33:22 -0700

    hid polygon

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -106,13 +106,13 @@ export class GoogleMap extends Component {
             }}
           />
 
-          <Polygon
+          {/* <Polygon
             paths={constants.TEST_COORDS}
             strokeColor="#0000FF"
             strokeOpacity={0.8}
             strokeWeight={2}
             fillColor="#0000FF"
-            fillOpacity={0.35} />
+            fillOpacity={0.35} /> */}
 
         </Map>
 
