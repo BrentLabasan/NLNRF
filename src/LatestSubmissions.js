78f384c324007eb4bb37617099aad74c836294ef
Brent Labasan <BrentLabasan@gmail.com>
2020-04-18 13:28:46 -0700

    sorting works

--- src/LatestSubmissions.js
+++ src/LatestSubmissions.js
@@ -1,27 +1,47 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 
 export default function LatestSubmissions(props) {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
 
-  const latestSubmissions = props.locations.map((loc) => {
-    return <li>
-        {loc.dateTime}
-        a
-    </li>
-  });
+    const style = {
+        padding: '10px',
+        border: '1px solid grey'
+    };
 
-  return (
-    <div>
-      {/* <p>You clicked {count} times</p>
+    function compare(a, b) {
+        // debugger;
+        console.log('HMMM', moment(a.dateTime).format(),  moment(b.dateTime).format());
+        console.log('return value', a.dateTime > b.dateTime ? 1 : -1);
+        return a.dateTime < b.dateTime ? 1 : -1;
+    }
+
+    // debugger;
+    // console.table(props.locations.slice(0, 10).sort(compare));
+    let sorted = props.locations.slice(0, 10).sort(compare);
+    const latestSubmissions = sorted.map((loc) => {
+        // debugger;
+        return <li style={style}>
+            {loc.nameDescr}
+            <br />
+            {moment(loc.dateTime).fromNow()}
+
+        </li>
+    });
+
+
+    return (
+        <div>
+            {/* <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button> */}
 
-      <ul>
-{latestSubmissions}
-      </ul>
+            <ul>
+                {latestSubmissions}
+            </ul>
 
-    </div>
-  );
+        </div>
+    );
 }
